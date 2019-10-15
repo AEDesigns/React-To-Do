bcfb6ebe4d3bf6c75467cb1ee7358786800cf727
Andrew Elliott <mtgelliott15@gmail.com>
2019-10-15 11:06:03 -0500

    Build out add/remove feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Todos from './todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        todos: [
+            {id: 1, content: 'Buy Milk'},
+            {id: 2, content: 'Watch Avengers'}
+        ]
+    }
+    deleteToDo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        })
+        this.setState({
+            todos
+        })
+    }
+    render() {
+        return (
+            <div className="todo-app container">
+                <h1 className="center blue-text">Todo's</h1>
+                <Todos todos={this.state.todos} deleteToDo={this.deleteToDo}/>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/todos.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Todos = ({todos, deleteToDo}) => {
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={() => {deleteToDo(todo.id)}}>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center"> You have no todo's left!</p>
+    )
+    return (
+        <div className="Todos collection">
+            {todoList}
+        </div>
+    )
+}
+
+export default Todos
